460f0d2585f36911187036c46c984a4d08c53acb
Risad Malik <72878885+Risad-Raihan@users.noreply.github.com>
2024-05-18 04:23:39 +0600

    Update Missing_value_handling.py

--- Missing_value_handling.py
+++ Missing_value_handling.py
@@ -6,4 +6,18 @@ features_with_na =[features for features in dateset.columns if dateset[features]
 for feature in features_with_na:
   print(feature, np.round(dateset[feature].isnull().mean(), 4), ' % missing values')
 
+# Here we need to see if the features of the null value are important or not and compare it with another feature like SalesPrice. It actually indicated and tell us whether we need 
+# to drop the null value from the data set or not and replace it 
 
+for feature in features_with_na:
+    data = dataset.copy()
+    
+    # let's make a variable that indicates 1 if the observation was missing or zero otherwise
+    data[feature] = np.where(data[feature].isnull(), 1, 0)
+    
+    # let's calculate the mean SalePrice where the information is missing or present
+    data.groupby(feature)['SalePrice'].median().plot.bar()
+    plt.title(feature)
+    plt.show()
+
+    
